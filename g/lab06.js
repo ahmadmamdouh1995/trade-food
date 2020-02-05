f1e50a23ee273df49f504aa3861860b4802bd626
ahmadmamdouh <mamdouh486@gmail.com>
2020-02-06 00:28:47 +0200

    kjklj

---
+++ g/lab06.js
@@ -0,0 +1,19 @@
+var name , age;
+
+alert("welcome");
+name = prompt ("what is your name ? ");
+//  if (   name == "ALi" ){
+    
+//     confirm(name);
+// }
+
+// document.write('<h3>' +"your_name is :" + name + '</h3>');
+age = prompt("what is your age ?");
+    document.write('<h4>' + ":" + name + '</h4>');
+    document.write('<h4>' + ":" + age + '</h4>');
+
+
+
+
+
+
