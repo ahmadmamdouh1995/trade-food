c3cfcac23cafa17abc2c7a4c6b3ea05199a00e6e
ahmadmamdouh <mamdouh486@gmail.com>
2020-02-06 09:48:47 +0200

    add for loop

--- g/lab06.js
+++ g/lab06.js
@@ -1,17 +1,35 @@
-var name , age;
+// var name , age;
+var name = prompt ("what is your name ? ");
+var age = prompt("what is your age ?");
+var numOfItem ;
+ function information (){
+    while (name !== "ahmad" && age !== 25 ) {
+      name = prompt ("please your name must ahmad ");
+      age = prompt ("your age must be 25");
+    }
+      alert ("you can choose 25 item only !! " );
 
-alert("welcome");
-name = prompt ("what is your name ? ");
-//  if (   name == "ALi" ){
-    
-//     confirm(name);
-// }
+     var  numOfitem = prompt("how many item you are listed ?");
+      for( var i=0 ; i <= numOfitem; i++ ){
+        document.write ('<h3>' + 'items =' + i +'</h3>');
 
-// document.write('<h3>' +"your_name is :" + name + '</h3>');
-age = prompt("what is your age ?");
-    document.write('<h4>' + ":" + name + '</h4>');
-    document.write('<h4>' + ":" + age + '</h4>');
 
+ 
+      }
+
+ 
+ //while (name === "ahmad" && age <= 25 ) {
+       // prompt("hello" + name +" ");
+        // document.write('<h3>' + "name =" + name  +'</h3>');
+        // document.write('<h3>' + "age =" + age +'</h3>');
+        // name = prompt ("what is your name ? ");
+        // age = prompt("what is your age ?");    
+    } 
+
+information()
+ 
+document.write ('<h3>' + 'name =' + name +'</h3>');
+document.write ('<h3>'+'age ='+ age  + '</h3>');
 
 
 
